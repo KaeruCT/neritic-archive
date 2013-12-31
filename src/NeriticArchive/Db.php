7b1080657aefd6b6209088720e0f1bb3802e914c
Andres V <darklink898@gmail.com>
2013-12-30 21:25:41 -0600

    removed charset from connection string

--- src/NeriticArchive/Db.php
+++ src/NeriticArchive/Db.php
@@ -11,7 +11,7 @@ class Db
     public function __construct(array $config)
     {
         $this->pdo = new PDO(
-            "mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8",
+            "mysql:host={$config['db_host']};dbname={$config['db_name']}",
             $config['db_user'],
             $config['db_pass']
         );
